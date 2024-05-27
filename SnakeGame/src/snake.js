e4bb0fe58276fdc337ee61eddd11ae3e617d8cc6
Josh Frederick <1joshfrederick@gmail.com>
2024-05-27 17:15:47 -0400

    [Update] snake.js
    -Added function to randomly place the food
    -Added color to the snake
    -Creating function to move Snake

--- SnakeGame/src/snake.js
+++ SnakeGame/src/snake.js
@@ -5,6 +5,14 @@ var columns = 20;
 var board;
 var context;
 
+// Snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+// Food
+var foodX;
+var foodY;
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = rows * blockSize;
@@ -12,10 +20,31 @@ window.onload = function(){
     // Used for drawing on the board
     context = board.getContext("2d"); 
     
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
     update();
 }
 
 function update(){
     context.fillStyle="black";
+    // X, Y coordinates, then width and height
     context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="blue";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+
+function changeDirection(){
+
+}
+
+
+function placeFood(){
+    // Math.random = number between 0-1
+    foodX = Math.floor(Math.random() * columns) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
